87b7fff11a148ffec01129cdd97c0cee8523c90e
Nhut <vivuvivunice@gmail.com>
2023-03-26 08:01:58 +0700

    init source and seperate 3 layers

--- Music.Net-App/Program.cs
+++ Music.Net-App/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Music.Net_App.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace Music.Net_App
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
