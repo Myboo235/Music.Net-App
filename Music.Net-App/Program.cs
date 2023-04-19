e7e459b37f01123a701f1c67729e09f20cc6d2a2
Việt Hoàng Công <102210241@sv1.dut.udn.vn>
2023-04-19 21:14:41 +0700

    Revert "assign BLL"
    This reverts commit c67ac536acc58c1a7dcb603312468228387e0e5f.

--- Music.Net-App/Program.cs
+++ Music.Net-App/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Music.Net_App.DAL.Viet;
 using Music.Net_App.View;
 using Music.Net_App.View.Listener;
 
