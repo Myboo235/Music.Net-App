2eaba93c4847289b17956269b706136bbf939db4
Huy <102210210@sv1.dut.udn.vn>
2023-04-20 20:47:46 +0700

    kkk

---
+++ Music.Net-App/BLL/SongBLL.cs
@@ -0,0 +1,21 @@
+﻿using Music.Net_App.DAL.Huy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Music.Net_App.BLL
+{
+    internal class SongBLL
+    {
+        public List<Song> getAllsongs()
+        {
+            using (MusicNetAppEntities1 db = new MusicNetAppEntities1())
+            {
+                List<Song> songs = db.Songs.ToList();
+                return songs;
+            }
+        }
+    }
+}
